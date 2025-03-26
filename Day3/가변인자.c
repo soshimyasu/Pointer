8529446f5b2895a15323d7dcff07c6a7b01c2ad8
soshimyasu <116233004+soshimyasu@users.noreply.github.com>
2025-03-26 17:20:42 +0900

    Update 가변인자.c

--- Day3/가변인자.c
+++ Day3/가변인자.c
@@ -75,3 +75,38 @@ void main(void)
 }
 
 #endif
+
+
+
+/***********************************************************/
+// [1-7-14] : my_add 함수의 재설계
+/***********************************************************/
+
+#if 01
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int my_add(int cnt, ...)
+{
+	int i, sum = 0;
+
+	va_list ap;
+	va_start(ap, cnt);
+
+	for (i = 1; i <= cnt; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+
+	va_end(ap);
+	return sum;
+}
+
+void main(void)
+{
+	printf("%d\n", my_add(3, 7, 5, 4));
+	printf("%d\n", my_add(5, 1, 2, 6, 9, 10));
+}
+
+#endif
