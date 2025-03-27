ef51fe6a066b7c199f4dc40fe3afe01e604c0b44
soshimyasu <116233004+soshimyasu@users.noreply.github.com>
2025-03-27 12:18:10 +0900

    Update Lecture9.c

--- Day4/Lecture9.c
+++ Day4/Lecture9.c
@@ -103,3 +103,264 @@ void main(void)
 }
 
 #endif
+
+
+/***********************************************************/
+// [1-9-5] : 비트필드의 멤버의 데이터 타입
+/***********************************************************/
+
+#if 0
+
+#include <stdio.h>
+
+struct st
+{
+	int a1 : 3;
+	int a2 : 2;
+	int a3 : 1;
+}x;
+
+void main(void)
+{
+	x.a3 = -1;
+
+	if (x.a3 == -1) printf("TRUE\n");
+	else printf("FALSE\n");
+
+	x.a3 = 1;
+
+	if (x.a3 == 1) printf("TRUE\n");
+	else printf("FALSE\n");
+}
+
+#endif
+
+/***********************************************************/
+// [1-9-6] : Unnamed 비트필드 멤버
+/***********************************************************/
+
+#if 0
+
+#include <stdio.h>
+
+volatile struct st
+{
+	unsigned short LSEON : 1;
+	unsigned short LSERDY : 1;
+	unsigned short LSEBYP : 1;
+	unsigned short rsvd1 : 5;
+	unsigned short RTCSEL : 2;
+	unsigned short rsvd2 : 5;
+	unsigned short RTCEN : 1;
+}RTC_CR;
+
+void main(void)
+{
+	printf("%d\n", sizeof(RTC_CR));
+}
+
+#endif
+
+#if 0
+
+#include <stdio.h>
+
+volatile struct st
+{
+	unsigned short LSEON : 1;
+	unsigned short LSERDY : 1;
+	unsigned short LSEBYP : 1;
+	unsigned short : 5;
+	unsigned short RTCSEL : 2;
+	unsigned short : 5;
+	unsigned short RTCEN : 1;
+}RTC_CR;
+
+void main(void)
+{
+	printf("%d\n", sizeof(RTC_CR));
+}
+
+#endif
+
+/***********************************************************/
+// [1-9-7] : Zero Size Width 비트필드
+/***********************************************************/
+
+#if 0
+
+#include <stdio.h>
+
+volatile struct st
+{
+	unsigned int a1 : 5;
+	unsigned int a2 : 25;
+	unsigned int a3 : 3;
+	unsigned int a4 : 4;
+}x = { 0x1f, 0x0, 0x7, 0x0 };
+
+void main(void)
+{
+	printf("%d\n", sizeof(x));
+	printf("0x%.8x\n", ((unsigned int *)&x)[0]);
+	printf("0x%.8x\n", ((unsigned int *)&x)[1]);
+}
+
+#endif
+
+#if 0
+
+#include <stdio.h>
+
+volatile struct st
+{
+	unsigned int a1 : 5;
+	unsigned int : 0;
+	unsigned int a2 : 25;
+	unsigned int : 0;
+	unsigned int a3 : 3;
+	unsigned int : 2;
+	unsigned int a4 : 4;
+}x = { 0x1f, 0x1fffff, 0x7, 0xf };
+
+void main(void)
+{
+	printf("%d\n", sizeof(x));
+	printf("0x%.8x\n", ((unsigned int *)&x)[0]);
+	printf("0x%.8x\n", ((unsigned int *)&x)[1]);
+	printf("0x%.8x\n", ((unsigned int *)&x)[2]);
+}
+
+#endif
+// 결과
+// 12
+// 0x0000001f
+// 0x001fffff
+// 0x000001e7
+
+
+/***********************************************************/
+// [1-9-8] : 공용체와 구조체
+/***********************************************************/
+
+#if 0
+
+#include <stdio.h>
+
+union float_data
+{
+	float f;
+
+	struct
+	{
+		unsigned int mant : 23;
+		unsigned int exp : 8;
+		unsigned int sign : 1;
+	}bit;
+}fdata;
+
+void main(void)
+{
+	fdata.f = 13.625;
+
+	printf("%f\n", fdata.f);
+	printf("sign(1bit):%x\n", fdata.bit.sign);
+	printf("exponent(8bit):%x\n", fdata.bit.exp);
+	printf("mantissa(23bit):%x\n", fdata.bit.mant);
+
+	fdata.bit.sign = 1;
+	printf("%f\n", fdata.f);
+}
+
+#endif
+// 결과
+// 13.625000
+// sign(1bit):0
+// exponent(8bit):82
+// mantissa(23bit):5a0000
+// -13.625000
+
+
+/***********************************************************/
+// [1-9-9] : 파일시스템의 파일 종류 인식
+/***********************************************************/
+
+#if 0
+
+#include <stdio.h>
+
+unsigned char buf[] = { 0x25, 0x0f };
+
+struct fat_file
+{
+	unsigned char r : 1;
+	unsigned char h : 1;
+	unsigned char s : 1;
+	unsigned char v : 1;
+	unsigned char d : 1;
+	unsigned char a : 1;
+};
+
+#define fn	((struct fat_file *)buf)
+
+void main(void)
+{
+	int i;
+
+	for (i = 0; i < 2; i++)
+	{
+		if ((fn[i].r && fn[i].h && fn[i].s && fn[i].v)
+			|| (fn[i].r && fn[i].h && fn[i].s && fn[i].v && fn[i].d && fn[i].a)) printf("Long File Name\n");
+		else printf("A[%d]D[%d]V[%d]S[%d]H[%d]R[%d]\n", fn[i].a, fn[i].d, fn[i].v, fn[i].s, fn[i].h, fn[i].r);
+	}
+}
+
+#endif
+
+/***********************************************************/
+// [1-9-10] : 효율적인 long file name의 판단
+/***********************************************************/
+
+#if 01
+
+#include <stdio.h>
+
+unsigned char buf[] = { 0x25, 0x0f, 0x3f };
+
+struct fat_file
+{
+	unsigned char r : 1;
+	unsigned char h : 1;
+	unsigned char s : 1;
+	unsigned char v : 1;
+	unsigned char d : 1;
+	unsigned char a : 1;
+	unsigned char : 0;
+};
+
+struct long_file
+{
+	unsigned char ln : 4;
+	unsigned char : 0;
+};
+
+union file
+{
+	struct fat_file n;
+	struct long_file l;
+};
+
+#define fn	((union file *)buf)
+
+void main(void)
+{
+	int i;
+
+	for (i = 0; i < 3; i++)
+	{
+		if (fn[i].l.ln == 0xf ) printf("Long File Name\n");
+		else printf("A[%d]D[%d]V[%d]S[%d]H[%d]R[%d]\n", fn[i].n.a, fn[i].n.d, fn[i].n.v, fn[i].n.s, fn[i].n.h, fn[i].n.r);
+	}
+}
+
+#endif
